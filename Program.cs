2b310674f01f42ef64524f17785dd766f1d91de0
Rodrigo Castagnaro <rodrigoskte@hotmail.com>
2023-09-27 23:16:49 -0300

    insights

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
+builder.Services.AddApplicationInsightsTelemetry();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
